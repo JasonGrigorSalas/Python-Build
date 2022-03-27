5defdd74e1a4fb7c7bf8bc63f1c39f74a9687e95
JasonGrigorSalas <jasoncoco1@gmail.com>
2022-03-26 21:35:41 -0400

    Added new file.

---
+++ variables.py
@@ -0,0 +1,14 @@
+# Comment in Python.
+print("My first Python file.")
+name = "Jason"
+age = 23
+eyecolor = "brown"
+haircolor = "darkbrown"
+
+dinosaurs = ["Allosaurus", "Stegosaurus", "Diplodocus", "Apatosaurus", "Brachiosaurus", "Dryosaurus", "Ornitholestes"]
+
+print("My name is " + name)
+print("My age is " + str(age))
+
+alternateName = input("What is your name?")
+print("My name is now " + alternateName)
